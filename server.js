6d1237c6cda4d60ec9370bce5fd5289cf583713f
haiaza <ajames1342@gmail.com>
2024-08-14 22:39:30 -0400

    Server Works

---
+++ server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
+const MONGO_URI = process.env.MONGO_URI
+const PORT = 3000
+
+mongoose.connect(MONGO_URI)
+mongoose.connection.on("connected", () =>{
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on port 3000')
+})
