d4e79a6ec8a58b07d8a633c96d020e963fe3d86f
haiaza <ajames1342@gmail.com>
2024-08-14 23:56:54 -0400

    Auth pending.Working on Views.

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ mongoose.connect(MONGO_URI)
 mongoose.connection.on("connected", () =>{
     console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
+mongoose.connection.on('error', () => {
+    console.log(error)
+})
 
 app.listen(PORT, () => {
     console.log('Listening on port 3000')
